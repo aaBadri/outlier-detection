3d2ad2d9fdcd85bf45de392e82df419b1cb49b89
AliAkbar Badri <aabadri74@gmail.com>
2017-10-22 18:22:53 +0330

    adds hash function

--- getVar.py
+++ getVar.py
@@ -1,4 +1,6 @@
 # TODO we do not check if center and a and b are in a line
+import random
+
 import numpy as np
 import pandas as pd
 
@@ -10,6 +12,22 @@ train = train.drop(8, axis=1)
 print("data is loaded")
 
 train = train[:100]
+
+
+def hash(train, hash_rate):
+    hashed_train = []
+    for i in range(0, hash_rate):
+        random_vector = []
+        for j in range(0, train.shape[0]):
+            random_vector[j] = random.randint(-1, 1)
+        hashed_train.append([])
+        for index, record in train.iterrows():
+            dotted = np.dot(record, random_vector)
+        if dotted >= 0:
+            hashed_train[i].append(1)
+        else:
+            hashed_train[i].append(-1)
+    return hashed_train
 
 
 def getABOF(vertex, a, b):
@@ -42,3 +60,4 @@ print("finish")
 train.to_csv("mammadAgha.csv", index=False)
 print(varABOF)
 print(train.head())
+
--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-# TODO we do not check if center and a and b are in a line
-import pandas as pd
+import random
 
-myList = [[0, 0, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0], [10, 10, 10, 0, 0, 0, 0, 0]]
-
-train_url = "./dump_data_10k_rep_0/ipsweep_normal.csv"
-train = pd.read_csv(train_url, delimiter=';', header=None)
-print(list(train.iloc[5]))
-# for i, row in train.iterrows():
-#     print(list(row[:]))
+import numpy as np
+info = [[1, -2, 3, -4],[0, 1, -2, 1],[1, 2, 3, 4],[-1, -2, -3, -4]]
+counter =0
+hashed_train = []
+for i in range(0, 3):
+    random_vector = []
+    for j in range(0, 4):
+        random_vector.append(random.randint(-1, 1))
+    print("random",random_vector)
+    hashed_train.append([])
+    for record in info:
+        dotted = np.dot(random_vector, record)
+        print("dotted ",dotted)
+        if dotted >= 0:
+            print("salam man")
+            hashed_train[i].append(1)
+        else:
+            print("salam an")
+            hashed_train[i].append(-1)
+print("counter",counter)
+print("hashed ",hashed_train)
